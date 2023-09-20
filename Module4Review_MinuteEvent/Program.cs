80c463c6bd276eba3f81dc31a6c837fd24a9f0f3
Kelsee Watts <watts.kelsee@gmail.com>
2023-09-20 06:29:13 -0500

    Add project files.

---
+++ Module4Review_MinuteEvent/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Module4Review_MinuteEvent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
